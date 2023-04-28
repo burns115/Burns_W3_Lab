5a2972296f6e7d8f5d4853cc8c54f9293ed7a9d9
burns115 <rburns@email.neit.edu>
2023-04-28 09:38:17 -0400

    adding new stuff

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,101 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => (
+  <div>
+    <MyAwesomeComponent />
+  </div>
+);
+
+const Box = ({ size = 'medium', backgroundColor = '#ccc', children }) => {
+  let width;
+  let height;
+
+  switch (size) 
+  {
+    case 'small':
+    case 'sm':
+    case 's':
+      width = '200px';
+      height = '200px';
+      break;
+
+    case 'medium':
+    case 'md':
+    case 'm':
+      width = '400px';
+      height = '400px';
+      break;
+
+    case 'large':
+    case 'lg':
+    case 'l':
+      width = '600px';
+      height = '600px';
+      break;
+
+    default:
+      width = '300px';
+      height = '300px';
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div style={{ width, height, backgroundColor }}>
+      {children}
+    </div>
+  );
+};
+
+const Text = ({ size = 'medium', color = '#000', children }) => 
+{
+  let fontSize;
+
+  switch (size) {
+    case 'small':
+    case 'sm':
+      fontSize = '12px';
+      break;
+
+    case 'medium':
+    case 'md':
+      fontSize = '16px';
+      break;
+
+    case 'large':
+    case 'lg':
+      fontSize = '20px';
+      break;
+
+    default:
+      fontSize = '16px';
+  }
+
+  return (
+    <p style={{ fontSize, color }}>
+      {children}
+    </p>
+  );
+};
+
+const Button = ({ color = '#ccc', onClick, children }) => (
+  <button style={{ backgroundColor: color }} onClick={onClick}>
+    {children}
+  </button>
+);
+
+const MyAwesomeComponent = () => (
+  <Box size="lg" backgroundColor="#fff">
+    
+    <Button color="black" onClick={() => alert('Congratulations!!')}>
+      Click me
+    </Button>
+
+    <Text size="lg" color="#000">
+      This text will get bigger or smaller!
+    </Text>
+
+  </Box>
+);
+
 
 export default App
